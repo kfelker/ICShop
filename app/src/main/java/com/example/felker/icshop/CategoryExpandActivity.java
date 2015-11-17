1f97c38beed51b3135488b694236e405012fb49e
kfelker <kevin-felker@uiowa.edu>
2015-11-17 09:15:40 -0600

    11/3

--- app/src/main/java/com/example/felker/icshop/CategoryExpandActivity.java
+++ app/src/main/java/com/example/felker/icshop/CategoryExpandActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,19 +20,37 @@ public class CategoryExpandActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_expand);
+
+        mainList = createData();
+        ExpandableListView listView = (ExpandableListView) findViewById(R.id.listView);
+        CategoryExpandAdapter adapter = new CategoryExpandAdapter(this,mainList);
+        listView.setAdapter(adapter);
+
+        listView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+
+                // selected item
+                //get the main group
+                MainRetailCategory headerInfo = mainList.get(groupPosition);
+                //get the child info
+                SubRetailCategory detailInfo =  headerInfo.getSubCatList().get(childPosition);
+                //display it or do something with it
+
+                //System.err.println("child clicked");
+                Toast.makeText(getApplicationContext(), detailInfo.toString()+ detailInfo.getID(), Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_category_expand, menu);
-
-        setContentView(R.layout.activity_category_expand);
-        mainList = createData();
-        ExpandableListView listView = (ExpandableListView) findViewById(R.id.listView);
-        CategoryExpandAdapter adapter = new CategoryExpandAdapter(this,mainList);
-        listView.setAdapter(adapter);
-
         return true;
     }
 
